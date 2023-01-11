72ac0982266a1e2575ca552583f328a20ff7d977
김현호 <hh.kim@gnewsoft.com>
2023-01-11 16:39:42 +0900

    feat: 초기 설정

---
+++ todo/src/components/Table.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<th class="id">ID</th>
+					<th class="comment">내용</th>
+					<th class="state">상태</th>
+					<th class="button">~~</th>
+				</tr>
+			</thead>
+		</table>
+		<tbody>
+			바디
+		</tbody>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
